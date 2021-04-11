0509f8a32b8231f0225e4e22cc4ac2b768b2b2a6
Alyssa Champion <alyssa.champion2016@gmail.com>
2021-04-11 00:20:52 -0700

    added CSS, JS, Index.html and note.html to repo

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+/* temp code for part one assignment*/
+
+
+/*first function = change page to dark mode*/
+
+/*function theme_changer(takes in switch_name) {
+    if (switch_name clicked)
+    doc - (set to dark mode)
+    else 
+    - keep page light mode
+}
+*/
+
+/* second function = save button*/
+
+/* function save_note ( take in the text) {
+    if save button is clicked
+    then save text 
+    (not sure if it should save into an array? or into a value?)
+} */
+
+/* third function = create new note*/
+
+/* function create_new {
+    if create button pressed
+    then text area will pop up 
+} */
